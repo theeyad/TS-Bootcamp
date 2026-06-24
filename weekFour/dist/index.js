c2c76f64073bd95e6c4df88dad6b41a30fff589f
theeyad <eyadosman35@gmail.com>
2026-06-24 19:01:30 +0300

    week four assignments

--- weekFour/dist/index.js
+++ weekFour/dist/index.js
@@ -1,15 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class User {
-    val;
-    constructor(val) {
-        this.val = val;
+// Write Class Code Here
+class Game {
+    title;
+    price;
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
     }
-    show(msg) {
-        console.log(`${msg}`);
+    getDiscount(discountValue) {
+        console.log(`The Discount Is ${discountValue}`);
     }
 }
-let userOne = new User(100);
-console.log(userOne.val);
-userOne.show("m");
+// Do Not Edit Here
+let gameOne = new Game("Ys", 100);
+let gameTwo = new Game(2064, 100); // There's A Game Called "2064"
+console.log(gameOne.title); // "Ys"
+console.log(gameOne.price); // 100
+gameOne.getDiscount("50"); // "The Discount Is 50"
+console.log(gameTwo.title); // 2064
+console.log(gameTwo.price); // 100
+gameTwo.getDiscount(80); // "The Discount Is 80"
 //# sourceMappingURL=index.js.map
--- weekFour/src/index.ts
+++ weekFour/src/index.ts
@@ -1,10 +1,22 @@
-class User<T> {
-    constructor(public val: T) { }
-    show(msg: T): void {
-        console.log(`${msg}`)
-    }
+// Write Class Code Here
+class Game<T> {
+  constructor(
+    public title: T,
+    public price: number,
+  ) {}
+  getDiscount(discountValue: T): void {
+    console.log(`The Discount Is ${discountValue}`);
+  }
 }
 
-let userOne = new User<number | string>(100)
-console.log(userOne.val)
-userOne.show("m")
+// Do Not Edit Here
+let gameOne = new Game<string>("Ys", 100);
+let gameTwo = new Game<number>(2064, 100); // There's A Game Called "2064"
+
+console.log(gameOne.title); // "Ys"
+console.log(gameOne.price); // 100
+gameOne.getDiscount("50"); // "The Discount Is 50"
+
+console.log(gameTwo.title); // 2064
+console.log(gameTwo.price); // 100
+gameTwo.getDiscount(80); // "The Discount Is 80"
